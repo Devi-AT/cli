21a3730d7e74dd2e1d059c8828abeead0cf7733c
Adrian Tanase <tanase.adrian92@gmail.com>
2022-05-02 15:58:17 +0300

    update the workflow file

--- index.js
+++ index.js
@@ -46,3 +46,21 @@ inquirer.prompt(QUESTIONS).then(answers => {
   }
   console.log(chalk.green('Project was created succsessfuly!'));
 });
+
+// async function downloadTestZip(archiveName) {
+//     // define what we want in the ZIP
+//     const code = await fetch("https://raw.githubusercontent.com/Touffy/client-zip/master/src/index.ts")
+//     const intro = { name: archiveName, lastModified: new Date(), input: `This is the ${archiveName}-zip library.` }
+  
+//     // get the ZIP stream in a Blob
+//     const blob = await downloadZip([intro, code]).blob()
+  
+//     // make and click a temporary link to download the Blob
+//     const link = document.createElement("a")
+//     link.href = URL.createObjectURL(blob)
+//     link.download = "test.zip"
+//     link.click()
+//     link.remove()
+  
+//     // in real life, don't forget to revoke your Blob URLs if you use them
+//   }
